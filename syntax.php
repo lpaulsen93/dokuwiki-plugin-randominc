24cf1d5fddef4e009b63e1dad60cd8be7166f1fd
LarsDW223 <lars_paulsen@web.de>
2017-07-30 10:55:33 +0200

    Merged the include plugin's render function into our render function. Now the include plugin can be called directly for the page inclusion and the duplicate code in helper.php could be deleted. The randominc specific flags 'max-width' and 'max-height' are still supported. Additionally all flags from the include plugin can be used now, e.g 'link'. Closes #2.

--- syntax.php
+++ syntax.php
@@ -36,7 +36,7 @@ class syntax_plugin_randominc extends DokuWiki_Syntax_Plugin {
 
         // break the pattern up into its constituent parts 
         list($include, $id, $section) = preg_split('/>|#/u', $match, 3); 
-        return array($include, $id, cleanID($section), explode('&', $flags)); 
+        return array($include, $id, cleanID($section), explode('&', $flags), $pos);
     }
 
     function _randompage($ns) {
@@ -72,67 +72,130 @@ class syntax_plugin_randominc extends DokuWiki_Syntax_Plugin {
         return @$num;
     }
 
-    function render($mode, Doku_Renderer $renderer, $data) {
+    /**
+     * Renders the included page(s)
+     *
+     * @author Michael Hamann <michael@content-space.de>
+     */
+    function render($format, Doku_Renderer $renderer, $data) {
         global $ID;
 
-        list($type, $raw_id, $section, $flags) = $data; 
+        // static stack that records all ancestors of the child pages
+        static $page_stack = array();
 
+        // when there is no id just assume the global $ID is the current id
+        if (empty($page_stack)) $page_stack[] = $ID;
+
+        $parent_id = $page_stack[count($page_stack)-1];
+        $root_id = $page_stack[0];
+
+        list($type, $raw_id, $section, $flags, $pos) = $data; 
         $id = $this->_applyMacro($raw_id);
-
-        // prevent caching if macros were used or some users are not allowed to read the page
-        $nocache = ($id != $raw_id);
-     
         resolve_pageid(getNS($ID), $id, $exists); // resolve shortcuts
         $ns=getNS($id.':dummy');    
-        // load the include class
-        $include =& plugin_load('helper', 'randominc');
-
-        $include->setMode($type);
-        $include->setFlags($flags);
-        $a_page=$this->_randompage($ns);
-        $an_id=$ns.':'.$a_page;
+        $page = $this->_randompage($ns);
+        $an_id=$ns.':'.$page;
         resolve_pageid(getNS($ID), $an_id, $exists); // resolve shortcuts
-        $ok = $include->setPage(array(
-            'id'      => $an_id,
-            'section' => $section,
-            'exists'  => $exists,
-            ));
-        if (!$ok) return false; // prevent recursion
+        $page = $an_id;
 
-        $nocache = ($nocache || (auth_aclcheck($an_id, '', array()) < AUTH_READ));
-
-        if ($mode == 'xhtml') {
-            if ($nocache) $renderer->info['cache'] = false;
-
-            // current section level
-            $clevel = 0;
-            preg_match_all('|<div class="level(\d)">|i', $renderer->doc, $matches, PREG_SET_ORDER);
-            $n = count($matches) - 1;
-            if ($n > -1) $clevel = $matches[$n][1];
-            $include->setLevel($clevel);
-            // close current section
-            if ($clevel && ($type == 'section')) {
-                $renderer->doc .= '</div>';
-            }
-            // include the page now
-            $include->renderXHTML($renderer);
-
-            // resume current section
-            if ($clevel && ($type == 'section')) {
-                $renderer->doc .= '<div class="level'.$clevel.'">';
-            }
-
-            return true;
-        } elseif ($mode == 'metadata') {
-            // for metadata renderer
-            if (!$flg_macro) $renderer->meta['relation']['haspart'][$an_id] = $exists;
-            // clear filechain - important!
-            $include->_clearPages();
-
-            return true;
+        // Map randominc syntax to include plugin mode
+        switch ($type)
+        {
+            case 'randomincsec':
+                $mode = 'section';
+                $sect = $section;
+            break;
+            default:
+                $mode = 'page';
+                $sect = NULL;
+            break;
         }
 
-        return false;  
+        // Get randominc specific flags, the rest will be passed to include plugin
+        $flagsarray = array();
+        $this->getFlags($flags, $flagsarray);
+
+        if (!$this->helper)
+            $this->helper = plugin_load('helper', 'include');
+        $flags = $this->helper->get_flags($flags);
+
+        $pages = $this->helper->_get_included_pages($mode, $page, $sect, $parent_id, $flags);
+
+        if ($format == 'metadata') {
+            /** @var Doku_Renderer_metadata $renderer */
+
+            // remove old persistent metadata of previous versions of the include plugin
+            if (isset($renderer->persistent['plugin_include'])) {
+                unset($renderer->persistent['plugin_include']);
+                unset($renderer->meta['plugin_include']);
+            }
+
+            $renderer->meta['plugin_include']['instructions'][] = compact('mode', 'page', 'sect', 'parent_id', $flags);
+            if (!isset($renderer->meta['plugin_include']['pages']))
+               $renderer->meta['plugin_include']['pages'] = array(); // add an array for array_merge
+            $renderer->meta['plugin_include']['pages'] = array_merge($renderer->meta['plugin_include']['pages'], $pages);
+            $renderer->meta['plugin_include']['include_content'] = isset($_REQUEST['include_content']);
+        }
+
+        $secids = array();
+        if ($format == 'xhtml' || $format == 'odt') {
+            $secids = p_get_metadata($ID, 'plugin_include secids');
+        }
+
+        // Embed page into an extra div for randominc's own max-width and max-height flags
+        if ($format == 'xhtml') {
+            $renderer->doc .= '<div class="entry-content" style="overflow: hidden;'.$flagsarray['max-width'].';'.$flagsarray['max-height'].'">'.DOKU_LF;
+        }
+
+        foreach ($pages as $page) {
+            extract($page);
+            $id = $page['id'];
+            $exists = $page['exists'];
+
+            if (in_array($id, $page_stack)) continue;
+            array_push($page_stack, $id);
+
+            // add references for backlink
+            if ($format == 'metadata') {
+                $renderer->meta['relation']['references'][$id] = $exists;
+                $renderer->meta['relation']['haspart'][$id]    = $exists;
+                if (!$sect && !$flags['firstsec'] && !$flags['linkonly'] && !isset($renderer->meta['plugin_include']['secids'][$id])) {
+                    $renderer->meta['plugin_include']['secids'][$id] = array('hid' => 'plugin_include__'.str_replace(':', '__', $id), 'pos' => $pos);
+                }
+            }
+
+            if (isset($secids[$id]) && $pos === $secids[$id]['pos']) {
+                $flags['include_secid'] = $secids[$id]['hid'];
+            } else {
+                unset($flags['include_secid']);
+            }
+
+            $instructions = $this->helper->_get_instructions($id, $sect, $mode, $level, $flags, $root_id, $secids);
+
+            if (!$flags['editbtn']) {
+                global $conf;
+                $maxseclevel_org = $conf['maxseclevel'];
+                $conf['maxseclevel'] = 0;
+            }
+            $renderer->nest($instructions);
+            if (isset($maxseclevel_org)) {
+                $conf['maxseclevel'] = $maxseclevel_org;
+                unset($maxseclevel_org);
+            }
+
+            array_pop($page_stack);
+        }
+
+        // When all includes have been handled remove the current id
+        // in order to allow the rendering of other pages
+        if (count($page_stack) == 1) array_pop($page_stack);
+
+        // Close our div
+        if ($format == 'xhtml') {
+            $renderer->doc .= '</div>'.DOKU_LF;
+        }
+
+        return true;
     }
 
 /* ---------- Util Functions ---------- */
@@ -140,7 +203,7 @@ class syntax_plugin_randominc extends DokuWiki_Syntax_Plugin {
     /**
      * Makes user or date dependent includes possible
      */
-    function _applyMacro($id) {
+    protected function _applyMacro($id) {
         global $INFO, $auth;
 
         $user     = $_SERVER['REMOTE_USER'];
@@ -156,6 +219,22 @@ class syntax_plugin_randominc extends DokuWiki_Syntax_Plugin {
             '@DAY@'   => date('d'),
         );
         return str_replace(array_keys($replace), array_values($replace), $id);
+    }
+
+    /**
+     * Get randominc specific flags
+     */
+    protected function getFlags($flags, &$flagsarray) {
+        foreach ($flags as $flag) {
+            if (!(strncmp($flag,'max-width',9) || strpos($flag,';')))
+            {
+                $flagsarray['max-width'] = $flag;
+            }
+            if (!(strncmp($flag,'max-height',10) || strpos($flag,';')))
+            {
+                $flagsarray['max-height'] = $flag;
+            }
+        }
     }
 }
 
