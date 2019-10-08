3fc0f4510dcfc300df2c8ff4c0c90f143a08b7ca
Bartek S <sadupl@gmail.com>
2019-10-08 20:06:00 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['readmore']              = '→Czytaj więcej...';
+$lang['nopagemsg']             = 'randominc: nie znaleziono strony.';
+$lang['plugin_include_failure'] = 'randominc: nie można załadować wtyczki. Zainstaluj lub włącz wtyczkę.';
---
+++ lang/pl/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['firstseconly']          = 'pokaż jedynie pierwszą sekcję dołączonych stron';
+$lang['showtaglogos']          = 'pokaż obrazek dla pierwszego tagu';
+$lang['showfooter']            = 'pokaż informacje poniżej o dołączonej stronie';
+$lang['showeditbtn']           = 'pokaż przycisk edycji';
