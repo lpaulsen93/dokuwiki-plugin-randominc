1ff381fa80171fd0f98ad867c7951abdc5a7c1e8
Franchesca <franchescaannis@arcor.de>
2020-10-13 13:40:05 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,8 +3,16 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Franchesca <franchescaannis@arcor.de>
  * @author Bartek S <sadupl@gmail.com>
  */
-$lang['readmore']              = '→Czytaj więcej...';
-$lang['nopagemsg']             = 'randominc: nie znaleziono strony.';
-$lang['plugin_include_failure'] = 'randominc: nie można załadować wtyczki. Zainstaluj lub włącz wtyczkę.';
+$lang['readmore']              = 'Hello! This is my first visit to your blog! We are a team of volunteers and starting a new initiative in a community in the same niche.
+Your blog provided us valuable information to work on. You have done a marvellous job!';
+$lang['nopagemsg']             = 'Hello! This is my first visit to your blog! We are a 
+team of volunteers and starting a new initiative in a community in the same niche.
+Your blog provided us valuable information to work on. You have done a marvellous 
+job!';
+$lang['plugin_include_failure'] = 'Hello! This is my first visit to your blog! We are a team of volunteers and 
+starting a new initiative in a community in the same niche.
+Your blog provided us valuable information to work on. You have done a marvellous 
+job!';
