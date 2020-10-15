adb5692396f3f935b71eab68dd69166ea95c52ba
Delia <deliaaslatt@web.de>
2020-10-15 18:15:05 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,8 +3,15 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Delia <deliaaslatt@web.de>
  * @author Bartek S <sadupl@gmail.com>
  */
-$lang['readmore']              = '→Czytaj więcej...';
-$lang['nopagemsg']             = 'randominc: nie znaleziono strony.';
-$lang['plugin_include_failure'] = 'randominc: nie można załadować wtyczki. Zainstaluj lub włącz wtyczkę.';
+$lang['readmore']              = 'Good day! I know this is kind of off topic but I was wondering if you knew where I could find a captcha plugin for my comment form?
+I\'m using the same blog platform as yours and I\'m having problems finding one?
+Thanks a lot!';
+$lang['nopagemsg']             = 'Good day! I know this is kind of off topic but I was wondering if you knew where I could find a captcha plugin for my 
+comment form? I\'m using the same blog platform as yours and I\'m having 
+problems finding one? Thanks a lot!';
+$lang['plugin_include_failure'] = 'Good day! I know this is kind of off topic but I was wondering if you knew where I could 
+find a captcha plugin for my comment form? I\'m using the same blog platform as yours 
+and I\'m having problems finding one? Thanks a lot!';
--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -3,9 +3,35 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Delia <deliaaslatt@web.de>
  * @author Bartek S <sadupl@gmail.com>
  */
-$lang['firstseconly']          = 'pokaż jedynie pierwszą sekcję dołączonych stron';
-$lang['showtaglogos']          = 'pokaż obrazek dla pierwszego tagu';
-$lang['showfooter']            = 'pokaż informacje poniżej o dołączonej stronie';
-$lang['showeditbtn']           = 'pokaż przycisk edycji';
+$lang['firstseconly']          = 'Good day! I know this is kind of off topic but I was wondering if 
+you knew where I could find a captcha plugin for my comment form?
+I\'m using the same blog platform as yours and I\'m having problems finding one?
+Thanks a lot!';
+$lang['showtaglogos']          = '20';
+$lang['showfooter']            = 'I would like to introduce myself to you, I am Kendrick Rentz. Virginia has usually been my dwelling location and I don\'t system on transforming it. To jog is what her family and her appreciate. He operates as a generation and planning officer. His wife and he keep a website. You may possibly want to verify it out: https://eiblogs.com/11-2/';
+$lang['showlink']              = 'Good day! I know this is kind of off topic but I was 
+wondering if you knew where I could find a captcha plugin for my 
+comment form? I\'m using the same blog platform as yours and I\'m having problems finding one?
+Thanks a lot!';
+$lang['showdate']              = 'Good day! I know this is kind of off topic but 
+I was wondering if you knew where I could find a captcha plugin for my 
+comment form? I\'m using the same blog platform as yours and I\'m 
+having problems finding one? Thanks a lot!';
+$lang['showuser']              = 'Good day! I know this is kind of off topic but I was wondering if you knew where I could find a captcha plugin for my comment form?
+I\'m using the same blog platform as yours and I\'m having problems finding one?
+Thanks a lot!';
+$lang['showcomments']          = 'Good day! I know this is kind of off topic but 
+I was wondering if you knew where I could find a captcha plugin for my comment form?
+I\'m using the same blog platform as yours and I\'m having problems finding one?
+Thanks a lot!';
+$lang['showlinkbacks']         = 'Good day! I know this is kind of off topic but I was wondering if you knew where I could find 
+a captcha plugin for my comment form? I\'m using the same blog platform as yours 
+and I\'m having problems finding one? Thanks a lot!';
+$lang['showtags']              = '13';
+$lang['showeditbtn']           = 'Good day! I know this is kind of off topic but I was wondering if you knew where 
+I could find a captcha plugin for my comment form? I\'m using the same blog platform as yours and I\'m 
+having problems finding one? Thanks a lot!';
+$lang['usernamespace']         = 'Aslatt';
