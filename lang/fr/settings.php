71b33b22be6cd539dc4ff4b90baa9efba4a458cc
Schplurtz le Déboulonné <schplurtz@laposte.net>
2022-06-07 20:25:16 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['firstseconly']          = 'Ne montrer que la première section des pages incluses.';
+$lang['showfooter']            = 'Afficher les information de page sous la page incluse.';
+$lang['showlink']              = 'Afficher les permaliens sous la page incluse.';
+$lang['showdate']              = 'Afficher les dates sous la page incluse.';
+$lang['showuser']              = 'Afficher les noms d\'utilisateur sous la page incluse.';
+$lang['showcomments']          = 'Afficher les commentaires sous la page incluse. Nécessite le greffon Discussion.';
+$lang['showlinkbacks']         = 'Afficher les rétroliens sous la page incluse. Nécessite le greffon Linkback.';
+$lang['showtags']              = 'Afficher les étiquettes sous la page incluse. Nécessite le greffon Tag.';
+$lang['showeditbtn']           = 'Afficher le bouton d\'édition';
+$lang['usernamespace']         = 'catégorie des pages utilisateur';
